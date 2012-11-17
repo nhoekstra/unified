dc84c4486d22e17444dc637805ee4cbd2c5d25c3
Nicholas Hoekstra <nhoekstr@sfu.ca>
2012-11-17 11:23:42 -0800

    Created initial project

---
+++ UniFied/UniFied/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  UniFied
+//
+//  Created by Nicholas Hoekstra on 11/17/12.
+//  Copyright (c) 2012 Renegades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ UniFied/UniFied/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UniFied
+//
+//  Created by Nicholas Hoekstra on 11/17/12.
+//  Copyright (c) 2012 Renegades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
