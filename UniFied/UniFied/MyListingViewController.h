c16f9c0736d7f665c38b6ac3c0b1201219369cb1
Nicholas Hoekstra <nhoekstr@infocmns27.ucslabs.sfu.ca>
2012-11-30 09:47:11 -0800

    Added user listing.

---
+++ UniFied/UniFied/MyListingViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyListingViewController.h
+//  UniFied
+//
+//  Created by Nicholas Hoekstra on 11/30/12.
+//  Copyright (c) 2012 Renegades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyListingViewController : UIViewController
+
+@end
---
+++ UniFied/UniFied/MyListingViewController.m
@@ -0,0 +1,43 @@
+//
+//  MyListingViewController.m
+//  UniFied
+//
+//  Created by Nicholas Hoekstra on 11/30/12.
+//  Copyright (c) 2012 Renegades. All rights reserved.
+//
+
+#import "MyListingViewController.h"
+
+@interface MyListingViewController ()
+
+@end
+
+@implementation MyListingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
