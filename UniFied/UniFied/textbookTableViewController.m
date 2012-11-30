b70a5339a1f3cc06412b2d9a9312f525b35f1a1f
Nicholas Hoekstra <nhoekstr@infocmns27.ucslabs.sfu.ca>
2012-11-30 10:27:32 -0800

    Connected everything together.

--- UniFied/UniFied/textbookTableViewController.m
+++ UniFied/UniFied/textbookTableViewController.m
@@ -44,7 +44,7 @@
     self.tableView.dataSource = self;
     
     //manually fill our table
-    totalStrings = [[NSMutableArray alloc] initWithObjects:@"Math Calculus", @"Discrete Math", @"History: The Western World", @"Understanding Poetry", @"Algorithm Design", @"myPosting", nil];
+    totalStrings = [[NSMutableArray alloc] initWithObjects:@"Math Calculus", @"Discrete Math", @"History: The Western World", @"Understanding Poetry", @"Algorithm Design", @"Java Walls and Mirrors", nil];
 }
 
 //This method is to handle the event where the user is searching in the search bar for a book
